92658bdc5eeeee7bb2f7dcab1b995db61750d0c6
Francis Inose <kizzycole10@gmail.com>
2021-07-12 11:46:14 +0100

    i hope this works

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import thunkMiddleware  from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; 
+import {searchRobots, requestRobots} from './reducers'
+import 'tachyons';
+import App from './containers/App';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({searchRobots, requestRobots})
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+  <App/>,
+  </Provider>,
   document.getElementById('root')
 );
 
